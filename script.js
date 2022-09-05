8b9950faa167e300d13f5111e209a0c34c7e67fe
rfanan <rofianan05@gmail.com>
2022-09-06 02:04:41 +0700

    text to speec converter with javascript (no api/library)

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
-console.log(ok);
+const textarea = document.querySelector('#text')
+let voicelist = document.querySelector('#voice')
+let speechbtn = document.querySelector('.submit')
+
+let synth = speechSynthesis
+let isSpeaking = true
+
+function voiceSpeech() {
+    for (let voice of synth.getVoices()) {
+        let option = document.createElement('option')
+        option.text = voice.name
+        voicelist.add(option)
+        console.log(option)
+    }
+}
+synth.addEventListener('voiceschanged', voiceSpeech)
+
+function texttospeech(text) {
+    let utternance = new SpeechSynthesisUtterance(text)
+    for (let voice of synth.getVoices()) {
+        if (voice.name === voicelist.nodeValue) {
+            utternance.voice = voice
+        }
+    }
+    speechSynthesis.speak(utternance)
+}
+
+speechbtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    if (textarea.value !== '') {
+        texttospeech(textarea.value)
+    }
+    if (textarea.value.length > 80) {
+        if (isSpeaking) {
+            synth.resume()
+            isSpeaking = false
+            speechbtn.innerHTML = 'Pause Speech'
+        } else {
+            synth.pause
+            isSpeaking = true
+            speechbtn.innerHTML = 'Resume Speech'
+        }
+        setInterval(() => {
+            if (!synth.speaking && !isSpeaking) {
+                isSpeaking = true
+                speechbtn.innerHTML = 'Convert to Speech'
+            }
+        })
+    } else {
+        speechbtn.innerHTML = 'Convert to Speech'
+    }
+})
